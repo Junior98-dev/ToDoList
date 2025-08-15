02216c89202555d4d95354b18a01f3bc668e84ba
Junior98-dev <kounkouddorianjunior@gmail.com>
2025-08-16 01:11:25 +0200

    Exercice 2, 1. Concatenation d'un String 1 à 10000 on utilisant +

--- ExerciceCoreAPI.java
+++ ExerciceCoreAPI.java
@@ -55,5 +55,24 @@ public class ExerciceCoreAPI {
 
     public static void main(String[] args) {
         //helloWorld("Hello World");
+
+        // Exercise 2: StringBuilder Performance Test
+
+        // TODO: Compare performance:
+        // 1. Concatenate numbers 1-10000 using String (+)
+        long startTime = System.currentTimeMillis();
+        String str = "";
+        for (int i = 0; i <= 10000; i++) {
+            str += i;
+        }
+        long endTime = System.currentTimeMillis();
+        long totalTime = endTime - startTime;
+        System.out.println("Total time : " + totalTime + " ms");
+
+
+        // 2. Concatenate numbers 1-10000 using StringBuilder
+        // - Measure time for both approaches using System.currentTimeMillis()
+        // - Print the time difference
+        // - Explain why there's a difference
     }
 }

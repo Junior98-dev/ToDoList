2551e49d2f023d451b1310df2e1dcc428b67a40f
Junior98-dev <kounkouddorianjunior@gmail.com>
2025-08-20 16:41:35 +0200

    Methode simpleSearch qui prend en paramettre un tableau et un nombre

--- Algorithmes.java
+++ Algorithmes.java
@@ -7,7 +7,9 @@ public class Algorithmes {
 
     // 1- Implementation d'Algorithme simple recherche (Deviner le nombre auquel je pense)
     public static int simpleSearch(ArrayList<Integer> tab, int number){
+        int low = 0;
         for(int a : tab){
+            low++;
             if(a == number){
                 return a;
             }
@@ -15,15 +17,26 @@ public class Algorithmes {
         return -1;
     }
 
+    public static int simpleSearch(int[] tab, int number){
+        for(int i = 0; i < tab.length; i++){
+            if(tab[i] == number){
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
          ArrayList<Integer> tab = new ArrayList<Integer>(100);
-         int number = (int)Math.floor(Math.random()*1000);
+         int[] tab2 = new int[100];
+         int number = (int)Math.floor(Math.random()*100);
 
-         for(int i = 1; i <= 100; i++){
+         for(int i = 0 ; i < 100; i++){
              tab.add(i);
+             tab2[i] += i;
          }
          System.out.println("Le nombre recherché : " + number);
-         System.out.println(simpleSearch(tab, number));
+         System.out.println(Algorithmes.simpleSearch(tab2, number));
 
 
 

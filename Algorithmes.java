20b296713a72c4298959892817464e5b120a8de0
Junior98-dev <kounkouddorianjunior@gmail.com>
2025-08-20 20:52:17 +0200

    benarySearch algo

--- Algorithmes.java
+++ Algorithmes.java
@@ -26,17 +26,43 @@ public class Algorithmes {
         return -1;
     }
 
+    public static int binarySearch(int[] tab, int number){
+
+        int left = 0;
+        int right = tab.length - 1;
+        int count = 0;
+
+        while(left <= right){
+            // recherche de l'index du mileu du tableau
+            int middle = (int)Math.floor((double) (left + right) / 2);
+            count++; // le nombre de tour pour trouver le number
+
+            if(tab[middle] == number){
+                System.out.println(count);
+                return middle;
+            }else if(tab[middle] < number){
+                left =  middle + 1; // On recherche number dans l'intervalle à droite
+            }else {
+                right = middle - 1; // On recherche number dans l'intervalle à gauche
+            }
+        }
+
+        return -1;
+    }
+
     public static void main(String[] args) {
          ArrayList<Integer> tab = new ArrayList<Integer>(100);
-         int[] tab2 = new int[100];
-         int number = (int)Math.floor(Math.random()*100);
+         int[] tab2 = new int[10000];
+         int number = (int)Math.floor(Math.random()*10000);
 
-         for(int i = 0 ; i < 100; i++){
+         for(int i = 0 ; i < 10000; i++){
              tab.add(i);
              tab2[i] += i;
          }
-         System.out.println("Le nombre recherché : " + number);
-         System.out.println(Algorithmes.simpleSearch(tab2, number));
+
+         System.out.println(number);
+         System.out.println(binarySearch(tab2, number));
+
 
 
 

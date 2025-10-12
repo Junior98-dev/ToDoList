7017c05d773b135396cf2ff72a79a4c2b900545b
Junior98-dev <kounkouddorianjunior@gmail.com>
2025-10-13 00:55:43 +0200

    Resolution de l'exercice 1 InventoryManager

--- execice/src/exercice_map/InventoryManager.java
+++ execice/src/exercice_map/InventoryManager.java
@@ -13,34 +13,44 @@ public class InventoryManager {
     // TODO: Ajouter un produit avec sa quantité
     public void addProduct(String productName, int quantity) {
         // À implémenter avec put()
+        inventory.put(productName, quantity);
     }
 
     // TODO: Obtenir la quantité d'un produit
     public Integer getQuantity(String productName) {
         // À implémenter avec get()
-        return null;
+        return inventory.get(productName);
     }
 
     // TODO: Retourner le nombre total de produits différents
     public int getTotalProducts() {
         // À implémenter avec size()
-        return 0;
+        return inventory.size();
     }
 
     // TODO: Vérifier si l'inventaire est vide
     public boolean isInventoryEmpty() {
         // À implémenter avec isEmpty()
-        return false;
+        return inventory.isEmpty();
     }
 
     // TODO: Vider complètement l'inventaire
     public void clearInventory() {
         // À implémenter avec clear()
+        inventory.clear();
     }
 
     // TODO: Afficher l'inventaire complet
     public void displayInventory() {
         // À implémenter
+        if (inventory == null || inventory.isEmpty()){
+            System.out.println("L'inventaire est vide.");
+        }else{
+            System.out.println("Inventaire complet :");
+            for (Map.Entry<String, Integer> entry : inventory.entrySet()){
+                System.out.println("- " + entry.getKey() + " : " + entry.getValue());
+            }
+        }
     }
 
     public static void main(String[] args) {
